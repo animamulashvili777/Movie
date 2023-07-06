9a5a0f79cd0ccc2bf5ac55d2a8bb47cdb77e9ab7
DESKTOP-LKPBV2D\Ani <anukam02@yahoo.com>
2023-07-06 13:38:38 +0400

    .

--- app/src/main/java/SignUpActivity.kt
+++ app/src/main/java/SignUpActivity.kt
@@ -19,15 +19,11 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
-        // Initialize views
         emailEditText = findViewById(R.id.editTextEmail)
         passwordEditText = findViewById(R.id.editTextPassword)
         confirmPasswordEditText = findViewById(R.id.editTextConfirmPassword)
         signUpButton = findViewById(R.id.buttonSignUp)
         passwordMismatchTextView = findViewById(R.id.passwordMismatchTextView)
-
-        // Set click listener for sign-up button
         signUpButton.setOnClickListener {
             signUp()
         }
@@ -39,16 +35,11 @@ class SignUpActivity : AppCompatActivity() {
         val confirmPassword = confirmPasswordEditText.text.toString()
 
         if (password == confirmPassword) {
-            // Perform your own sign-up logic here
-            // For example, you can display a success message using Toast:
             Toast.makeText(this, "Sign up successful.", Toast.LENGTH_SHORT).show()
-            // Redirect to the Home activity or perform any other desired actions
-            // For example, you can start the Home activity using an Intent:
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
-            finish() // Optional: finish the Sign Up activity to prevent going back
+            finish()
         } else {
-            // Passwords don't match, display error message
             passwordMismatchTextView.visibility = View.VISIBLE
         }
     }
